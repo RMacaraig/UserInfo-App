912b0b7f976706141578eff77c31e6cff035afff
rylemacaraig <rylemacaraig@gmail.com>
2019-09-15 23:56:53 -0400

    Initial Commit

---
+++ assignment1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment1
+//
+//  Created by Ryle  Macaraig on 2019-09-15.
+//  Copyright © 2019 Ryle  Macaraig. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
